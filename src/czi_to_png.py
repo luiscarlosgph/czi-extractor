cc50a0c14dbc19fc506eade9529350b2e7f9565b
Luis C. Garcia Peraza Herrera <luis@teide.local>
2023-10-06 16:01:07 +0200

    czi extractor is now working.

--- src/czi_to_png.py
+++ src/czi_to_png.py
@@ -1,52 +1,84 @@
 """
-@brief   Convert a folder of CZI images containing z-stacks into a folder of PNG images.
+@brief   Extract all the slices of a z-stack in CZI format into a folder of PNG images.
 @author  Luis C. Garcia Peraza Herrera (luiscarlos.gph@gmail.com).
 @date    5 Oct 2023.
 """
 import os
 import czifile
-import pillow
+import PIL.Image
 import argparse
+import numpy as np
 
 
-def convert_czi_to_png(input_dir, output_dir):
+def hex_to_dec(hex_color):
+    # Remove the "#" if present in the input string
+    if hex_color.startswith("#"):
+        hex_color = hex_color[1:]
+
+    # Extract individual components (RR, GG, BB, AA) from the hex string
+    alpha = int(hex_color[0:2], 16)
+    red = int(hex_color[2:4], 16)
+    green = int(hex_color[4:6], 16)
+    blue = int(hex_color[6:8], 16)
+
+    return red, green, blue, alpha
+
+
+def convert_czi_to_png(input_czi_path, output_dir):
     # Create the output folder if it doesn't exist
     os.makedirs(output_dir, exist_ok=True)
 
-    # List all files in the input folder
-    czi_files = [f for f in os.listdir(input_dir) if f.endswith('.czi')]
+    # Get z-stack image with shape (C, Z, H, W)
+    czi_data = np.squeeze(czifile.imread(input_czi_path)[0][0], axis=-1)
 
-    # Loop through each CZI file in the input folder
-    for czi_file in czi_files:
-        # Construct the full path of the input CZI file
-        input_czi_path = os.path.join(input_dir, czi_file)
+    # Get metadata
+    metadata = czifile.CziFile(input_czi_path).metadata(raw=False)
 
-        # Open the CZI file
-        with czifile.CziFile(input_czi_path) as czi:
-            # Read the CZI data
-            czi_data = czi.asarray()
+    # Loop through each slice and save it as a PNG image
+    fname = os.path.splitext(os.path.basename(input_czi_path))[0]
+    for c in range(czi_data.shape[0]):
+        # Get the color of the channel
+        red, green, blue, alpha = hex_to_dec(metadata['ImageDocument']['Metadata']['DisplaySetting']['Channels']['Channel'][c]['Color'])
+        color_name = metadata['ImageDocument']['Metadata']['DisplaySetting']['Channels']['Channel'][c]['Name']
 
-        # Loop through each slice and save it as a PNG image
-        for i, slice_data in enumerate(czi_data):
+        # Loop over the z-stack
+        for z in range(czi_data.shape[1]):
             # Construct the output PNG file path with slice number suffix
-            output_png_path = os.path.join(output_dir, f"{os.path.splitext(czi_file)[0]}_slice_{i}.png")
+            output_png_path = os.path.join(output_dir, f"{fname}_color_{color_name}_slice_{z}.png")
+            
+            # Get image for the current color channel and stack depth
+            im_gray = czi_data[c][z].astype(np.float32) / 255.
+            im_rgb = np.stack([im_gray] * 3, axis=-1)
+            im_rgb[:, :, 0] *= red 
+            im_rgb[:, :, 1] *= green
+            im_rgb[:, :, 2] *= blue 
+            im_rgb = np.clip(np.round(im_rgb).astype(np.uint8), 0, 255)
 
             # Convert the slice data to a PNG image using Pillow
-            img = pillow.Image.fromarray(slice_data)
-            img.save(output_png_path)
+            im = PIL.Image.fromarray(im_rgb)
+            im.save(output_png_path)
 
-
-def main():
+def parse_cmdline():
     # Read command line arguments
     parser = argparse.ArgumentParser(description="Convert CZI images to PNG format with slice number suffixes.")
     parser.add_argument("--input", required=True, help="Path to the input directory containing CZI images.")
     parser.add_argument("--output", required=True, help="Path to the output directory for saving PNG images.")
     args = parser.parse_args()
+    
+    # Sanity check: make sure that output folder does not exist
+    if os.path.exists(args.output) and os.path.isdir(args.output):  
+        raise RuntimeError("[ERROR] The output directory already exists. Provide an output path that does not exist.")
+    return args
+
+
+def main():
+    # Parse command line parameters
+    args = parse_cmdline()    
 
     # Perform the conversion
     convert_czi_to_png(args.input, args.output)
 
-    print("Conversion complete. PNG images saved in the output folder.")
+    print("[INFO] Conversion complete. PNG images saved in the output folder.")
 
 
 if __name__ == "__main__":
